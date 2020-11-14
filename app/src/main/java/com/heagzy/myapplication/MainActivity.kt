9e451c78bf917ee4e9a277776f707bbc4828f6aa
Ahmed Hegazi <engahmedali2022@gmail.com>
2020-11-13 20:49:43 -0800

    bg_rounded_add_task

--- app/src/main/java/com/heagzy/myapplication/MainActivity.kt
+++ app/src/main/java/com/heagzy/myapplication/MainActivity.kt
@@ -1,18 +1,55 @@
 package com.heagzy.myapplication
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.wear.ambient.AmbientModeSupport
+import com.heagzy.myapplication.databinding.ActivityMainBinding
 
-class MainActivity : WearableActivity() {
+
+class MainActivity : FragmentActivity(), AmbientModeSupport.AmbientCallbackProvider {
+
+
+    lateinit var db: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        // Enables Always-on
-        setAmbientEnabled()
 
+//        setContentView(R.layout.activity_main)
+        db = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        setOnClickListener()
 
 
     }
+
+    private fun setOnClickListener() {
+        db.fabAddTask.setOnClickListener {
+            addNewTask()
+        }
+    }
+
+    private fun addNewTask() {
+
+    }
+
+    override fun getAmbientCallback(): AmbientModeSupport.AmbientCallback {
+        return MyAmbientCallback()
+    }
+
+    private class MyAmbientCallback : AmbientModeSupport.AmbientCallback() {
+
+        override fun onEnterAmbient(ambientDetails: Bundle?) {
+            super.onEnterAmbient(ambientDetails)
+
+        }
+
+        override fun onExitAmbient() {
+            super.onExitAmbient()
+        }
+    }
+
+
 }
